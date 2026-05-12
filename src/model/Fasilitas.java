75829c399d4599b8efc5b3f8b327d18be16daf5d
JodySiahaan <jodyalfonsosiahaan14@gmail.com>
2026-05-12 20:22:31 +0700

    039_Jody

--- src/model/Fasilitas.java
+++ src/model/Fasilitas.java
@@ -1,3 +1,5 @@
+// 12S24039 - Jody Alfonso Siahaan
+
 package model;
 
 public class Fasilitas extends Entity {
--- src/model/Penghuni.java
+++ src/model/Penghuni.java
@@ -1,3 +1,5 @@
+// 12S24039 - Jody Alfonso Siahaan
+
 package model;
 
 public class Penghuni extends Entity {
